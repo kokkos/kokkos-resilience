76df26f0f35ee7f11a4959a4263d328af8063082
Nicolas Morales <nmmoral@sandia.gov>
2019-08-27 17:15:02 -0700

    api: add some more filesystem utility functions and link boost filesystem since we're already linking boost

---
+++ src/resilience/filesystem/Filesystem.hpp
@@ -0,0 +1,13 @@
+#ifndef INC_RESILIENCE_FILESYSTEM_FILESYSTEM_HPP
+#define INC_RESILIENCE_FILESYSTEM_FILESYSTEM_HPP
+
+#include <string>
+#include <cstdint>
+
+namespace KokkosResilience
+{
+  std::uintmax_t remove_all( const std::string &_path );
+  bool create_directory( const std::string &_path );
+}
+
+#endif  // INC_RESILIENCE_FILESYSTEM_FILESYSTEM_HPP
