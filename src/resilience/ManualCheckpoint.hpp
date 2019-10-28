aa0a85667d61205e0e1dd1ea86f5a9c71cfdd47b
jeffmiles63 <jsmiles@apollo.sandia.gov>
2019-10-28 12:48:05 -0600

    changes to move chkpt mirror logic to resilience space

--- src/resilience/ManualCheckpoint.hpp
+++ src/resilience/ManualCheckpoint.hpp
@@ -2,7 +2,10 @@
 #define INC_RESILIENCE_MANUALCHECKPOINT_HPP
 
 #ifdef KOKKOS_ENABLE_MANUAL_CHECKPOINT
-   #include "resilience/stdio/StdFileSpace.hpp"
+
+   #include <ResilienceViewCopy.hpp>
+
+   #include "stdio/StdFileSpace.hpp"
 
    #ifdef KR_ENABLE_HDF5
       #include "hdf5/HDF5Space.hpp"
