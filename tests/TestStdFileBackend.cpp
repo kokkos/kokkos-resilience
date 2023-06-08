b7982c44a25eb307adc523232e71fdfd09c8cc32
Nicolas Morales <nmmoral@sandia.gov>
2023-06-08 16:26:04 -0700

    #48: disable stdfile backend until we update it

--- tests/TestStdFileBackend.cpp
+++ tests/TestStdFileBackend.cpp
@@ -40,10 +40,10 @@
  */
 #include "TestCommon.hpp"
 
-#include <resilience/stdfile/StdFileBackend.hpp>
+#include <resilience/backend/stdfile/StdFileBackend.hpp>
 #include <resilience/AutomaticCheckpoint.hpp>
-#include <resilience/StdFileContext.hpp>
-#include <resilience/filesystem/Filesystem.hpp>
+#include <resilience/context/StdFileContext.hpp>
+#include <resilience/backend/filesystem/Filesystem.hpp>
 
 #include <string>
 
