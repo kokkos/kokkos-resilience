c09d0167dc628ed249dd3cd95ba5228d25e3a4b6
eagiem <eagiem@sandia.gov>
2025-05-15 11:21:26 -0700

    Remove fix for Kokkos::initialize scopeguard conflict with googletest

--- tests/TestMain.cpp
+++ tests/TestMain.cpp
@@ -50,12 +50,9 @@ main( int argc, char **argv )
 #if defined(KR_ENABLE_HDF5_PARALLEL) || defined(KR_ENABLE_VELOC_BACKEND)
   MPI_Init( &argc, &argv );
 #endif
-  bool ret;
 
   Kokkos::initialize( argc, argv );
-  {
-    ret = RUN_ALL_TESTS();
-  }
+  auto  ret = RUN_ALL_TESTS();
   Kokkos::finalize();
 
 #if defined(KR_ENABLE_HDF5_PARALLEL) || defined(KR_ENABLE_VELOC_BACKEND)
